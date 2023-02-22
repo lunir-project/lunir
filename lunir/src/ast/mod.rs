6e9064b469030d73939e43145dba0ca79f90cea9
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-02-22 13:27:17 +0300

    Removed tests from mod.rs files

--- lunir/src/ast/mod.rs
+++ lunir/src/ast/mod.rs
@@ -22,35 +22,6 @@
 
 pub mod expression;
 pub mod statement;
+mod tests;
 pub mod tree;
-
 pub mod visitors;
-
-#[cfg(test)]
-mod tests {
-    use super::expression::{BinaryExpressionKind, IdentifierString};
-
-    #[test]
-    fn sanitization() {
-        assert_eq!(
-            String::from("test\n\n\n\x12").sanitize_special(),
-            "test\\n\\n\\n\\18"
-        );
-        assert_eq!(
-            String::from("hello-world I'm  0x90__!").sanitize_identifier(),
-            "hello_world_Im_0x90_"
-        );
-    }
-
-    #[test]
-    fn compound_conversion() {
-        assert_eq!(
-            BinaryExpressionKind::Add.to_compound(),
-            BinaryExpressionKind::AddAssign
-        );
-        assert_eq!(
-            BinaryExpressionKind::DivAssign.to_compound(),
-            BinaryExpressionKind::DivAssign
-        );
-    }
-}
