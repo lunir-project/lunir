96286b29e236d131c71c960644274fa3fcc15e49
AmJayden <cosbyemails@gmail.com>
2023-02-25 14:38:07 -0500

    Documented instructions and added JumpNot

--- src/il/mod.rs
+++ src/il/mod.rs
@@ -22,12 +22,14 @@
 
 use std::collections::HashMap;
 
+/// Represents the two states of a table, array (index-value pairs) and hashmap (key-value pairs).
 #[derive(Debug, Clone)]
 pub enum Table {
     Map(HashMap<Value, Value>),
     Array(Vec<Value>),
 }
 
+/// Represents the types of operands that can be used within an IL instruction.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Value {
     Nil,
@@ -37,6 +39,7 @@ pub enum Value {
     StackIndex(usize),
 }
 
+/// Represents the types of values that can be present in the constant table.
 #[derive(Debug, Clone)]
 pub enum Constant {
     Nil,
@@ -47,25 +50,29 @@ pub enum Constant {
     Table(Table),
 }
 
+/// A load operation with a destination index and a source value.
 #[derive(Debug, Clone)]
 pub struct Load {
     pub dest: usize,
     pub src: Value,
 }
 
+/// A get global operation with a destination index and a source constant table index.
 #[derive(Debug, Clone)]
 pub struct GetGlobal {
     pub dest: usize,
     pub constant: usize,
 }
 
+/// Perfoms a table index operation on the value at stack index `source` using the value at stack index `key`, then stores the result in stack index `dest`.
 #[derive(Debug, Clone)]
 pub struct GetTable {
     pub dest: usize,
     pub source: usize,
-    pub index: Value,
+    pub key: Value,
 }
 
+/// Represents the kinds of supported binary operations.
 #[derive(Debug, Clone)]
 pub enum BinaryOpKind {
     Add,
@@ -77,15 +84,17 @@ pub enum BinaryOpKind {
     Sub,
 }
 
+/// A binary operation with an operator, a destination index, and left and right operands.
 #[derive(Debug, Clone)]
 pub struct BinaryOp {
-    pub ind: BinaryOpKind,
+    pub operator: BinaryOpKind,
     pub dest: usize,
 
     pub left: Value,
     pub right: Value,
 }
 
+/// Represents the kinds of supported unary operations.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub enum UnaryOpKind {
     Len,
@@ -93,16 +102,18 @@ pub enum UnaryOpKind {
     Neg,
 }
 
+/// A unary operation with an operator, a destination index, and a single operand.
 #[derive(Debug, Clone)]
 pub struct UnaryOp {
-    pub kind: UnaryOpKind,
+    pub operator: UnaryOpKind,
     pub dest: usize,
 
-    pub value: Value,
+    pub left: Value,
 }
 
+/// Represents the kinds of supported conditions.
 #[derive(Debug, Clone)]
-pub enum ConditionalKind {
+pub enum ConditionKind {
     Eq,
     Ge,
     Gt,
@@ -114,15 +125,17 @@ pub enum ConditionalKind {
     Or,
 }
 
+// A condition with a kind, a destination index, and left and right operands.
 #[derive(Debug, Clone)]
-pub struct Conditional {
-    pub kind: ConditionalKind,
+pub struct Condition {
+    pub kind: ConditionKind,
     pub dest: usize,
 
     pub left: Value,
     pub right: Value,
 }
 
+/// Information about a program-counter-relative-jump, `start` is the program counter, `end` is the offset added to the base index, and `offset` is the total number of instructions skipped for the branch.
 #[derive(Debug, Clone)]
 pub struct JumpBranch {
     pub start: usize,
@@ -130,18 +143,27 @@ pub struct JumpBranch {
     pub offset: isize,
 }
 
-// Unconditional jump
+/// An unconditional jump.
 #[derive(Debug, Clone)]
 pub struct Jump {
     pub branch: JumpBranch,
 }
 
+/// A conditional jump that only jumps if the `NOT` of the value at stack index `cond` evaluates to true.
+#[derive(Debug, Clone)]
+pub struct JumpNot {
+    pub branch: JumpBranch,
+    pub cond: usize,
+}
+
+/// A jump with an attached condition.
 #[derive(Debug, Clone)]
 pub struct ConditionalJump {
     pub branch: JumpBranch,
-    pub condition: Conditional,
+    pub condition: Condition,
 }
 
+/// Creates a new table at stack index `dest` with an initial size of `table_size` and `array_size` array elements.
 #[derive(Debug, Clone)]
 pub struct NewTable {
     pub dest: usize,
@@ -150,12 +172,14 @@ pub struct NewTable {
     pub array_size: usize,
 }
 
+/// Represents either a number or a variable number of values that approach the top of the stack.
 #[derive(Debug, Clone)]
 pub enum OptVariable {
     Variable,
     Number(usize),
 }
 
+/// Calls the function at stack index `callee` with `num_args` ahead of it on the stack, then returns a `num_returns` number of results.
 #[derive(Debug, Clone)]
 pub struct Call {
     pub callee: usize,
@@ -164,25 +188,27 @@ pub struct Call {
     pub num_returns: OptVariable,
 }
 
+/// Performs a return from the current chunk, passing all values fr om stack index `result_start` up to `result_start + result_count` to the caller.
 #[derive(Debug, Clone)]
 pub struct Return {
     pub result_start: usize,
     pub result_count: usize,
 }
 
-#[repr(u8)]
+/// Describes the arity of a function.
 #[derive(Debug, Clone)]
-pub enum VarargTag {
-    HasArg = 1,
-    IsVararg = 2,
-    NeedsArg = 4,
+pub enum Vararg {
+    HasArg,
+    IsVararg,
+    NeedsArg,
 }
 
+/// A function in LUNIR intermediate language.
 #[derive(Debug, Clone)]
 pub struct Function {
     pub constants: Vec<Constant>,
     pub code: Vec<u8>,
-    pub is_variadic: VarargTag,
+    pub is_variadic: Vararg,
     pub lineinfo: Vec<u32>,
     pub name: Option<String>,
     pub upvalue_count: u8,
@@ -190,6 +216,7 @@ pub struct Function {
     pub max_stack_size: u8,
 }
 
+/// All possible LUNIR intermediate language instructions.
 #[derive(Debug, Clone)]
 pub enum Instruction {
     Load(Box<Load>),
@@ -201,6 +228,7 @@ pub enum Instruction {
     UnaryOp(Box<UnaryOp>),
 
     Jump(Box<Jump>),
+    JumpNot(Box<JumpNot>),
     ConditionalJump(Box<ConditionalJump>),
 
     NewTable(Box<NewTable>),
@@ -209,5 +237,6 @@ pub enum Instruction {
     Call(Box<Call>),
 }
 
-#[derive(Debug)]
+/// A chunk of code in LUNIR's intermediate language.
+#[derive(Clone, Debug)]
 pub struct IlChunk(Vec<Instruction>);
