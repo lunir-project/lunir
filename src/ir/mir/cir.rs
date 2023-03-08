6879592245967c196c1f2917378240fe438ec2be
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-09 02:50:59 +0300

    Add splitting and unique insert

--- src/ir/mir/cir.rs
+++ src/ir/mir/cir.rs
@@ -1,11 +1,15 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
 use petgraph::{
+    data::Build,
     dot::{Config, Dot},
+    graph::NodeIndex,
     prelude::DiGraph,
+    stable_graph::IndexType,
+    Direction,
 };
 
-use crate::prelude::{IlChunk, Instruction};
+use crate::prelude::{IlChunk, Instruction, Jump};
 
 #[derive(Default)]
 pub(crate) struct CirGraph(DiGraph<IlChunk, bool, usize>);
@@ -22,6 +26,58 @@ impl CirGraph {
     pub(crate) fn new() -> Self {
         Self::default()
     }
+}
+
+fn graph_get_or_insert(
+    graph: &mut DiGraph<IlChunk, bool, usize>,
+    weight: IlChunk,
+) -> NodeIndex<usize> {
+    match graph
+        .node_weights()
+        .zip(graph.node_indices())
+        .find(|&(node_weight, _)| node_weight == &weight)
+    {
+        Some((_, idx)) => idx,
+        None => graph.add_node(weight).index().into(),
+    }
+}
+
+fn split_blocks(
+    blocks: &HashMap<usize, &[Instruction]>,
+    src_block_index: usize,
+    target_block_index: usize,
+    graph: &mut DiGraph<IlChunk, bool, usize>,
+    is_negated: bool,
+) -> NodeIndex<usize> {
+    let src_block = blocks.get(&src_block_index).unwrap();
+    for (&block_index, &curr_block) in blocks
+        .iter()
+        .filter(|&(&block_index, _)| block_index < target_block_index)
+    {
+        if (block_index..block_index + src_block.len()).contains(&target_block_index) {
+            let relative_target_index = target_block_index - block_index;
+
+            let src_node_index = graph_get_or_insert(graph, IlChunk::from(*src_block));
+
+            let leading_block_index =
+                graph_get_or_insert(graph, IlChunk::from(&curr_block[..relative_target_index]));
+
+            let trailing_block_index =
+                graph_get_or_insert(graph, IlChunk::from(&curr_block[relative_target_index..]));
+
+            graph.add_edge(
+                leading_block_index.into(),
+                trailing_block_index.into(),
+                true && !is_negated,
+            );
+
+            graph.add_edge(src_node_index.into(), trailing_block_index.into(), true);
+
+            return src_node_index;
+        }
+    }
+
+    panic!("Branch attempts to jump to invalid location");
 }
 
 pub(crate) fn into_cir_graph(instructions: Vec<Instruction>) {
@@ -46,199 +102,79 @@ pub(crate) fn into_cir_graph(instructions: Vec<Instruction>) {
 
     let graph = cfg.inner_mut();
 
-    for (pc, block) in blocks.iter() {
-        match (block.last(), block.len()) {
-            (Some(Instruction::Jump(data)), len) => {
-                let target = data.branch.end;
+    for (pc, src_block) in blocks.iter() {
+        match src_block.last() {
+            Some(Instruction::Jump(data)) => {
+                let target_block_index = data.branch.end;
 
-                match blocks.get(&target) {
+                match blocks.get(&target_block_index) {
                     Some(target_block) => {
-                        let from = graph
-                            .add_node(IlChunk::from(
-                                *blocks.get(&dbg!(pc)).unwrap(),
-                            ))
-                            .index();
-                        let to = graph.add_node(IlChunk::from(*target_block)).index();
+                        let from_block = graph_get_or_insert(graph, IlChunk::from(*src_block));
 
-                        graph.add_edge(from.into(), to.into(), true);
+                        let to_block = graph_get_or_insert(graph, IlChunk::from(*target_block));
+
+                        graph.add_edge(from_block, to_block, true);
                     }
                     None => {
-                        blocks
-                            .iter()
-                            .filter(|&(&k, _)| k < target)
-                            .find_map(|(&k, &v)| {
-                                if dbg!(dbg!(k..k + len).contains(dbg!(&target))) {
-                                    let relative_target_index = target - k;
-
-                                    let original_source =
-                                        graph.add_node(IlChunk::from(*block)).index();
-
-                                    let leading_block = graph
-                                        .add_node(IlChunk::from(&v[..relative_target_index]))
-                                        .index();
-
-                                    let trailing_block = graph
-                                        .add_node(IlChunk::from(&v[relative_target_index..]))
-                                        .index();
-
-                                    graph.add_edge(
-                                        leading_block.into(),
-                                        trailing_block.into(),
-                                        true,
-                                    );
-
-                                    graph.add_edge(
-                                        original_source.into(),
-                                        trailing_block.into(),
-                                        true,
-                                    );
-
-                                    Some(())
-                                } else {
-                                    None
-                                }
-                            })
-                            .expect("Branch attempts to jump to invalid location");
+                        split_blocks(&blocks, *pc, target_block_index, graph, false);
                     }
                 };
             }
-            (Some(Instruction::ConditionalJump(data)), len) => {
-                let target = data.branch.end;
 
-                match blocks.get(&target) {
+            Some(Instruction::ConditionalJump(data)) => {
+                let target_block_index = data.branch.end;
+
+                let source = match blocks.get(&target_block_index) {
                     Some(target_block) => {
-                        let from = graph
-                            .add_node(IlChunk::from(
-                                *blocks.get(&dbg!(pc)).unwrap(),
-                            ))
-                            .index();
-                        let to = graph.add_node(IlChunk::from(*target_block)).index();
+                        let original_source = graph_get_or_insert(graph, IlChunk::from(*src_block));
 
-                        graph.add_edge(from.into(), to.into(), true);
+                        let to = graph_get_or_insert(graph, IlChunk::from(*target_block));
 
-                        let non_divergent = blocks.get(&(data.branch.start + 1)).unwrap();
+                        graph.add_edge(original_source.into(), to.into(), true);
 
-                        let next = graph.add_node(IlChunk::from(*non_divergent)).index();
-
-                        graph.add_edge(from.into(), next.into(), false);
+                        original_source
                     }
                     None => {
-                        blocks
-                            .iter()
-                            .filter(|&(&k, _)| k < target)
-                            .find_map(|(&k, &v)| {
-                                
+                        let original_source =
+                            split_blocks(&blocks, *pc, target_block_index, graph, false);
 
-                                if dbg!(dbg!(k..k + len).contains(dbg!(&target))) {
-                                    let relative_target_index = target - k;
-
-                                    let original_source =
-                                        graph.add_node(IlChunk::from(*block)).index();
-
-                                    let leading_block = graph
-                                        .add_node(IlChunk::from(&v[..relative_target_index]))
-                                        .index();
-
-                                    let trailing_block = graph
-                                        .add_node(IlChunk::from(&v[relative_target_index..]))
-                                        .index();
-
-                                    graph.add_edge(
-                                        leading_block.into(),
-                                        trailing_block.into(),
-                                        true,
-                                    );
-
-                                    graph.add_edge(
-                                        original_source.into(),
-                                        trailing_block.into(),
-                                        true,
-                                    );
-
-                                    let non_divergent =
-                                        blocks.get(&(data.branch.start + 1)).unwrap();
-
-                                    let next =
-                                        graph.add_node(IlChunk::from(*non_divergent)).index();
-
-                                    graph.add_edge(original_source.into(), next.into(), false);
-
-                                    Some(())
-                                } else {
-                                    None
-                                }
-                            })
-                            .expect("Branch attempts to jump to invalid location");
+                        original_source
                     }
                 };
+
+                let non_divergent = blocks.get(&(data.branch.start + 1)).unwrap();
+
+                let next = graph_get_or_insert(graph, IlChunk::from(*non_divergent));
+
+                graph.add_edge(source, next, false);
             }
-            (Some(Instruction::JumpNot(data)), len) => {
-                let target = data.branch.end;
 
-                match blocks.get(&target) {
+            Some(Instruction::JumpNot(data)) => {
+                let target_block_index = data.branch.end;
+
+                let source = match blocks.get(&target_block_index) {
                     Some(target_block) => {
-                        let from = graph
-                            .add_node(IlChunk::from(
-                                *blocks.get(&(pc)).unwrap(),
-                            ))
-                            .index();
-                        let to = graph.add_node(IlChunk::from(*target_block)).index();
+                        let original_source = graph_get_or_insert(graph, IlChunk::from(*src_block));
 
-                        graph.add_edge(from.into(), to.into(), false);
+                        let to = graph_get_or_insert(graph, IlChunk::from(*target_block));
 
-                        let non_divergent = blocks.get(&(data.branch.start + 1)).unwrap();
+                        graph.add_edge(original_source.into(), to.into(), false);
 
-                        let next = graph.add_node(IlChunk::from(*non_divergent)).index();
-
-                        graph.add_edge(from.into(), next.into(), true);
+                        original_source
                     }
                     None => {
-                        blocks
-                            .iter()
-                            .filter(|&(&k, _)| k < target)
-                            .find_map(|(&k, &v)| {
-                                if dbg!(dbg!(k..k + len).contains(dbg!(&target))) {
-                                    let relative_target_index = target - k;
+                        let original_source =
+                            split_blocks(&blocks, *pc, target_block_index, graph, true);
 
-                                    let original_source =
-                                        graph.add_node(IlChunk::from(*block)).index();
-
-                                    let leading_block = graph
-                                        .add_node(IlChunk::from(&v[..relative_target_index]))
-                                        .index();
-
-                                    let trailing_block = graph
-                                        .add_node(IlChunk::from(&v[relative_target_index..]))
-                                        .index();
-
-                                    graph.add_edge(
-                                        leading_block.into(),
-                                        trailing_block.into(),
-                                        true,
-                                    );
-
-                                    graph.add_edge(
-                                        original_source.into(),
-                                        trailing_block.into(),
-                                        false,
-                                    );
-
-                                    let non_divergent =
-                                        blocks.get(&(data.branch.start + 1)).unwrap();
-
-                                    let next =
-                                        graph.add_node(IlChunk::from(*non_divergent)).index();
-
-                                    graph.add_edge(original_source.into(), next.into(), true);
-
-                                    Some(())
-                                } else {
-                                    None
-                                }
-                            })
-                            .expect("Branch attempts to jump to invalid location");
+                        original_source.into()
                     }
                 };
+
+                let non_divergent = blocks.get(&(data.branch.start + 1)).unwrap();
+
+                let next = graph_get_or_insert(graph, IlChunk::from(*non_divergent));
+
+                graph.add_edge(source, next, true);
             }
             _ => continue,
         }
@@ -248,19 +184,12 @@ pub(crate) fn into_cir_graph(instructions: Vec<Instruction>) {
 
     // dbg!(graph);
 
-    println!(
-        "{:#?}",
-        Dot::with_config(graph, &[])
-    );
+    println!("{:#?}", Dot::with_config(graph, &[]));
 }
 
 impl From<IlChunk> for CirGraph {
     fn from(chunk: IlChunk) -> Self {
         let mut cfg = CirGraph::new();
-
-        let graph = cfg.inner_mut();
-
-        let instructions = chunk.inner();
 
         Self::default()
     }
